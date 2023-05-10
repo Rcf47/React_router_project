cc36fced4bd5316ed81859f0fa7bb8c63693ff91
Vadim Cherepanov <xenos26@gmail.com>
2023-05-10 21:42:25 +0700

    feat: add project with two pages home and about

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import './Navbar.css'
+import { Link } from "react-router-dom"
+
+function Navbar() {
+  return (
+    <header>
+
+      <Link className='site-logo' to={"/"}>
+        #VANLIFE
+      </Link>
+      <nav className="navbar">
+        <Link to={"/about"}>About</Link>
+        <Link to={"/vans"}>Vans</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
