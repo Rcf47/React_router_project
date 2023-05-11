0f4fe59a756dc79fc005b48c647fc422a613ae6f
Vadim Cherepanov <xenos26@gmail.com>
2023-05-11 23:09:52 +0700

    fix: fix Link to VanDetail page

--- src/components/VanCard.jsx
+++ src/components/VanCard.jsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom"
+
 function VanCard(props) {
   return (
     <div key={props.key} className="van-tile">
-      <img src={props.img} />
-      <div className="van-info">
-        <h3>{props.name}</h3>
-        <p>${props.price}<span>/day</span></p>
-      </div>
-      <i className={`van-type ${props.type} selected`}>{props.type}</i>
+      <Link to={`${props.key}`}>
+
+        <img src={props.img} />
+        <div className="van-info">
+          <h3>{props.name}</h3>
+          <p>${props.price}<span>/day</span></p>
+        </div>
+        <i className={`van-type ${props.type} selected`}>{props.type}</i>
+      </Link >
     </div>
   )
 }
