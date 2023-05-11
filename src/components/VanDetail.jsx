27dcb0a80a67f614ce0cf7a44571efcfc2edeebe
Vadim Cherepanov <xenos26@gmail.com>
2023-05-12 06:19:47 +0700

    fix: fix bug with img in VanDetail

--- src/components/VanDetail.jsx
+++ src/components/VanDetail.jsx
@@ -15,6 +15,7 @@ function VanDetail() {
     <div className="van-detail-container">
       {van ? (
         <div className="van-detail">
+          <img src={van.imageUrl} />
           <i className={`van-type ${van.type} selected`}>{van.type}</i>
           <h2>{van.name}</h2>
           <p className="van-price"><span>${van.price}</span>/day</p>
