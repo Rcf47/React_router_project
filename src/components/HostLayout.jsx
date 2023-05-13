e22a535677e70b45325fd7b6790f3144c681c9fc
Vadim Cherepanov <xenos26@gmail.com>
2023-05-13 22:39:22 +0700

    chore: add relative link path

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,22 +1,43 @@
-import { NavLink, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom";
 
 function HostLayout() {
   const activeHostNavLink = {
-    fontWeight: 'bold',
-    textDecoration: 'underline',
-    color: '#161616'
-  }
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
   return (
     <>
       <nav className="host-nav">
-        <NavLink style={({ isActive }) => isActive ? activeHostNavLink : null} end to="/host">Dashboard</NavLink>
-        <NavLink style={({ isActive }) => isActive ? activeHostNavLink : null} to="/host/income">Income</NavLink>
-        <NavLink style={({ isActive }) => isActive ? activeHostNavLink : null} to="/host/vans">Vans</NavLink>
-        <NavLink style={({ isActive }) => isActive ? activeHostNavLink : null} to="/host/reviews">Reviews</NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeHostNavLink : null)}
+          end
+          to="."
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeHostNavLink : null)}
+          to="income"
+        >
+          Income
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeHostNavLink : null)}
+          to="vans"
+        >
+          Vans
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeHostNavLink : null)}
+          to="reviews"
+        >
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
     </>
-  )
+  );
 }
 
-export default HostLayout
+export default HostLayout;
