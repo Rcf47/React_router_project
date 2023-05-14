939171ade68c841fc76d82fd96b44718c4a2fb6f
Vadim Cherepanov <xenos26@gmail.com>
2023-05-14 22:20:18 +0700

    feat: add activeStyle for host van detail nav

--- src/pages/Host/HostVansDetail.jsx
+++ src/pages/Host/HostVansDetail.jsx
@@ -5,6 +5,12 @@ function HostVansDetail() {
   const { id } = useParams()
   const [currentVan, setCurrentVan] = useState(null)
 
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616"
+  }
+
   useEffect(() => {
     fetch(`/api/host/vans/${id}`)
       .then(res => res.json())
@@ -16,36 +22,34 @@ function HostVansDetail() {
   }
 
   return (
-    <>
-      <section>
-        <Link
-          to=".."
-          relative="path"
-          className="back-button"
-        >&larr; <span>Back to all vans</span></Link>
+    <section>
+      <Link
+        to=".."
+        relative="path"
+        className="back-button"
+      >&larr; <span>Back to all vans</span></Link>
 
-        <div className="host-van-detail-layout-container">
-          <div className="host-van-detail">
-            <img src={currentVan.imageUrl} />
-            <div className="host-van-detail-info-text">
-              <i
-                className={`van-type van-type-${currentVan.type}`}
-              >
-                {currentVan.type}
-              </i>
-              <h3>{currentVan.name}</h3>
-              <h4>${currentVan.price}/day</h4>
-            </div>
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={currentVan.imageUrl} />
+          <div className="host-van-detail-info-text">
+            <i
+              className={`van-type van-type-${currentVan.type}`}
+            >
+              {currentVan.type}
+            </i>
+            <h3>{currentVan.name}</h3>
+            <h4>${currentVan.price}/day</h4>
           </div>
         </div>
-      </section>
-      <nav>
-        <NavLink to="." end>Details</NavLink>
-        <NavLink to="pricing">Pricing</NavLink>
-        <NavLink to="photos">Photos</NavLink>
-      </nav>
-      <Outlet />
-    </>
+        <nav className="host-van-detail-nav">
+          <NavLink to="." end style={({ isActive }) => isActive ? activeStyles : null}>Details</NavLink>
+          <NavLink to="pricing" style={({ isActive }) => isActive ? activeStyles : null}>Pricing</NavLink>
+          <NavLink to="photos" style={({ isActive }) => isActive ? activeStyles : null}>Photos</NavLink>
+        </nav>
+        <Outlet />
+      </div>
+    </section>
   )
 }
 
