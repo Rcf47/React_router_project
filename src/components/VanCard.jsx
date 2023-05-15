e312431cc1034286ad9ef42884efefa35fedc922
Vadim Cherepanov <xenos26@gmail.com>
2023-05-15 22:42:59 +0700

    chore: add search parameters for map in VanCard

--- src/components/VanCard.jsx
+++ src/components/VanCard.jsx
@@ -4,7 +4,6 @@ function VanCard(props) {
   return (
     <div id={props.id} className="van-tile">
       <Link to={`${props.id}`}>
-
         <img src={props.img} />
         <div className="van-info">
           <h3>{props.name}</h3>
--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,38 +1,42 @@
 import { useEffect, useState } from "react";
 import VanCard from "../../components/VanCard";
+import { useSearchParams } from "react-router-dom";
 
 function Vans() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [allCards, setAllCards] = useState([]);
+  const typeFilter = searchParams.get("type");
+
+  const vansElements = allCards.map((item) => {
+    return (
+      <VanCard
+        key={item.id}
+        id={item.id}
+        img={item.imageUrl}
+        name={item.name}
+        price={item.price}
+        description={item.description}
+        type={item.type}
+      />
+    );
+  });
+
+  console.log(vansElements)
+  const displayedVans = typeFilter
+    ? vansElements.filter((char) => char.props.type.toLowerCase() === typeFilter)
+    : vansElements;
+
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
-      .then((data) =>
-        setAllCards(
-          data.vans.map((item) => {
-            console.log(item)
-            return (
-              < VanCard
-                key={item.id}
-                id={item.id}
-                img={item.imageUrl}
-                name={item.name}
-                price={item.price}
-                description={item.description}
-                type={item.type}
-              />
-            );
-          })
-        )
-      );
+      .then((data) => setAllCards(data.vans));
   }, []);
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
-      <div className="van-list">
-        {allCards}
-      </div>
+      <div className="van-list">{displayedVans}</div>
     </div>
-  )
+  );
 }
 
 export default Vans;
