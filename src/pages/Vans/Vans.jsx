a9e6ed09b6297ae77f86a681dc85da2c7dff3bce
Vadim Cherepanov <xenos26@gmail.com>
2023-05-16 23:09:56 +0700

    feat: add conditional selected class

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -21,9 +21,11 @@ function Vans() {
     );
   });
 
-  console.log(vansElements)
+  console.log(vansElements);
   const displayedVans = typeFilter
-    ? vansElements.filter((char) => char.props.type.toLowerCase() === typeFilter)
+    ? vansElements.filter(
+      (char) => char.props.type.toLowerCase() === typeFilter
+    )
     : vansElements;
 
   useEffect(() => {
@@ -31,16 +33,50 @@ function Vans() {
       .then((res) => res.json())
       .then((data) => setAllCards(data.vans));
   }, []);
+
+  function handleFilterChange(key, value) {
+    setSearchParams((prev) => {
+      if (value === null) {
+        prev.delete(key);
+      } else {
+        prev.set(key, value);
+      }
+      return prev;
+    });
+  }
+
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
-      <button onClick={() => setSearchParams({ type: "simple" })} className="van-type simple">Simple</button>
-      <button onClick={() => setSearchParams({ type: "luxury" })} className="van-type luxury">Luxury</button>
-      <button onClick={() => setSearchParams({ type: "rugged" })} className="van-type rugged">Rugged</button>
-      {typeFilter ? (<button onClick={() => setSearchParams({})} className="van-type clear-filters">Clear filter</button>) : null
-      }
+      <button
+        onClick={() => handleFilterChange("type", "simple")}
+        className={`van-type simple ${typeFilter === "simple" ? "selected" : null
+          }`}
+      >
+        Simple
+      </button>
+      <button
+        onClick={() => handleFilterChange("type", "luxury")}
+        className={`van-type luxury ${typeFilter === "luxury" ? "selected" : null}`}
+      >
+        Luxury
+      </button>
+      <button
+        onClick={() => handleFilterChange("type", "rugged")}
+        className={`van-type rugged ${typeFilter === "rugged" ? "selected" : null}`}
+      >
+        Rugged
+      </button>
+      {typeFilter ? (
+        <button
+          onClick={() => handleFilterChange("type", null)}
+          className={`van-type clear-filters`}
+        >
+          Clear filter
+        </button>
+      ) : null}
       <div className="van-list">{displayedVans}</div>
-    </div >
+    </div>
   );
 }
 
