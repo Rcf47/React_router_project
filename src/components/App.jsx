c9bf36d75ab70ab22c50bac232238a2d1c4c5572
Vadim Cherepanov <xenos26@gmail.com>
2023-05-18 18:09:30 +0700

    feat: add RouterProvider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import "../server"
@@ -17,30 +17,31 @@ import HostVanPhotos from '../pages/Host/HostVanPhotos'
 import HostVanInfo from '../pages/Host/HostVanInfo'
 import NotFound from '../pages/NotFound'
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route path="vans" element={<Vans />} />
+    <Route path="vans/:id" element={<VanDetail />} />
+    <Route path="host" element={<HostLayout />}>
+      <Route index element={<Dashboard />} />
+      <Route path="income" element={<Income />} />
+      <Route path="vans" element={<HostVans />} />
+      <Route path="vans/:id" element={<HostVansDetail />} >
+        <Route index element={<HostVanInfo />} />
+        <Route path="pricing" element={<HostVanPricing />} />
+        <Route path="photos" element={<HostVanPhotos />} />
+      </Route>
+      <Route path="reviews" element={<Reviews />} />
+    </Route>
+    <Route path="*" element={<NotFound />} />
+  </Route>
+
+))
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="vans" element={<Vans />} />
-          <Route path="vans/:id" element={<VanDetail />} />
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVansDetail />} >
-              <Route index element={<HostVanInfo />} />
-              <Route path="pricing" element={<HostVanPricing />} />
-              <Route path="photos" element={<HostVanPhotos />} />
-            </Route>
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>)
+    <RouterProvider router={router} />
+  )
 }
 
 export default App
