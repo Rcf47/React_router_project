4fdf4058c9da813ea6835ebcd8a45cdcd9373ba4
Vadim Cherepanov <xenos26@gmail.com>
2023-05-23 18:10:07 +0700

    feat: add try catch for Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLoaderData, Form, redirect } from "react-router-dom";
+import { useLoaderData, Form, redirect, useActionData } from "react-router-dom";
 import { loginUser } from "../api";
 
 export async function loader({ request }) {
@@ -11,25 +11,29 @@ export async function action({ request }) {
   const formData = await request.formData()
   const email = formData.get("email")
   const password = formData.get("password")
-  const data = await loginUser({ email, password })
-  localStorage.setItem("loggedin", true)
-  const response = redirect("/host")
-  response.body = true
-  return response
+  try {
+    const data = await loginUser({ email, password })
+    localStorage.setItem("loggedin", true)
+
+    const response = redirect("/host")
+    response.body = true
+    return response
+  } catch (error) {
+    return error.message
+  }
 }
 
 export default function Login() {
   const [status, setStatus] = useState("idle");
-  const [error, setError] = useState(null);
   const message = useLoaderData();
-
+  const actionMessage = useActionData()
 
 
   return (
     <div className="login-container">
       <h1>Sign in to your account</h1>
       {message && <h3 className="red">{message}</h3>}
-      {error ? <h3 className="red">{error.message}</h3> : null}
+      {actionMessage ? <h3 className="red">{actionMessage}</h3> : null}
       <Form method="post" className="login-form" replace>
         <input
           name="email"
