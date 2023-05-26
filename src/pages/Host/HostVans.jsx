42d9a14ee087f65df052d2fb55b4d10ca30d3b8a
Vadim Cherepanov <xenos26@gmail.com>
2023-05-26 20:57:28 +0700

    chore: my try add defer and await in HostVans

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,41 +1,46 @@
-import { Link, useLoaderData } from "react-router-dom"
+import { Await, Link, defer, useLoaderData } from "react-router-dom"
 import { getHostVans } from "../../api"
 import { requireAuth } from "../../utils"
+import { Suspense } from "react"
 
 
 export async function loader({ request }) {
   await requireAuth(request)
-  return getHostVans()
+  return defer({ vans: getHostVans() })
 }
 export default function HostVans() {
-  const vans = useLoaderData()
-
-
-  const hostVansEls = vans.map(van => (
-    <Link
-      to={van.id}
-      key={van.id}
-      className="host-van-link-wrapper"
-    >
-      <div className="host-van-single" key={van.id}>
-        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-        <div className="host-van-info">
-          <h3>{van.name}</h3>
-          <p>${van.price}/day</p>
-        </div>
-      </div>
-    </Link>
-  ))
+  const vansPromise = useLoaderData()
 
   return (
     <section>
       <h1 className="host-vans-title">Your listed vans</h1>
-      <div className="host-vans-list">
-        <section>
-          {hostVansEls}
-        </section>
+      <Suspense fallback={<h2>Loading vans...</h2>}>
+        <Await resolve={vansPromise}>
+          {vans => {
+            const hostVansEls = vansPromise.map(van => (
+              <Link
+                to={van.id}
+                key={van.id}
+                className="host-van-link-wrapper"
+              >
+                <div className="host-van-single" key={van.id}>
+                  <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                  <div className="host-van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                  </div>
+                </div>
+              </Link>
+            ));
+            < div className="host-vans-list" >
+              <section>
+                {hostVansEls}
+              </section>
 
-      </div>
-    </section>
+            </div>
+          }}
+        </Await>
+      </Suspense>
+    </section >
   )
 }
