e2ba9258ca2f2c71097cd8e02f7fab29e1bc34e2
Vadim Cherepanov <xenos26@gmail.com>
2023-05-27 12:12:51 +0700

    chore: add await defer and suspense in VanDetail

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,34 +1,45 @@
-import { Link, useLocation, useLoaderData } from "react-router-dom"
+import { Link, useLocation, useLoaderData, defer, Await } from "react-router-dom"
 import { getVans } from "../../api.js"
+import { Suspense } from "react"
 
 export function loader({ params }) {
-  return getVans(params.id)
+  return defer({ van: getVans(params.id) })
 }
 function VanDetail() {
   const location = useLocation()
-  const van = useLoaderData()
+  const vanPromise = useLoaderData()
 
 
   const search = location.state?.search || ""
   const type = location.state?.type || "all"
 
-  return (
-    <div className="van-detail-container">
-      <Link
-        to={`..${search}`}
-        relative="path"
-        className="back-button"
-      >&larr; <span>Back to {`${type}`} vans</span></Link>
-      <div className="van-detail">
-        <img src={van.imageUrl} />
-        <i className={`van-type ${van.type} selected`}>{van.type}</i>
-        <h2>{van.name}</h2>
-        <p className="van-price"><span>${van.price}</span>/day</p>
-        <p>{van.description}</p>
-        <button className="link-button">Rent this van</button>
-      </div>
+  function renderVanDetail(van) {
+    console.log(van.van)
+    return (
+      <div className="van-detail-container">
+        <Link
+          to={`..${search}`}
+          relative="path"
+          className="back-button"
+        >&larr; <span>Back to {`${type}`} vans</span></Link>
+        <div className="van-detail">
+          <img src={van.imageUrl} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price"><span>${van.price}</span>/day</p>
+          <p>{van.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
 
-    </div>
+      </div>
+    )
+  }
+  return (
+    <Suspense fallback={<h2>Loading van...</h2>}>
+      <Await resolve={vanPromise.van}>
+        {renderVanDetail}
+      </Await>
+    </Suspense>
   )
 }
 
