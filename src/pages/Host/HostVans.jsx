fe5316c6c5e6cff1437e006219e1752e66a891c1
Vadim Cherepanov <xenos26@gmail.com>
2023-05-27 11:53:00 +0700

    chore: add defer await suspense in HostVans

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -9,36 +9,38 @@ export async function loader({ request }) {
   return defer({ vans: getHostVans() })
 }
 export default function HostVans() {
-  const vansPromise = useLoaderData()
+  const dataPromise = useLoaderData()
 
+  function renderVanElements(vans) {
+    const hostVansEls = vans.map(van => (
+      <Link
+        to={van.id}
+        key={van.id}
+        className="host-van-link-wrapper"
+      >
+        <div className="host-van-single" key={van.id}>
+          <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+          <div className="host-van-info">
+            <h3>{van.name}</h3>
+            <p>${van.price}/day</p>
+          </div>
+        </div>
+      </Link>
+    ));
+    return (
+      < div className="host-vans-list" >
+        <section>
+          {hostVansEls}
+        </section>
+      </div>
+    )
+  }
   return (
     <section>
       <h1 className="host-vans-title">Your listed vans</h1>
       <Suspense fallback={<h2>Loading vans...</h2>}>
-        <Await resolve={vansPromise}>
-          {vans => {
-            const hostVansEls = vansPromise.map(van => (
-              <Link
-                to={van.id}
-                key={van.id}
-                className="host-van-link-wrapper"
-              >
-                <div className="host-van-single" key={van.id}>
-                  <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-                  <div className="host-van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}/day</p>
-                  </div>
-                </div>
-              </Link>
-            ));
-            < div className="host-vans-list" >
-              <section>
-                {hostVansEls}
-              </section>
-
-            </div>
-          }}
+        <Await resolve={dataPromise.vans}>
+          {renderVanElements}
         </Await>
       </Suspense>
     </section >
