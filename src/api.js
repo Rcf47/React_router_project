583d3c35515c7893d06b8a00a8059daf49e148b3
Vadim Cherepanov <xenos26@gmail.com>
2023-05-29 12:45:11 +0700

    chore: add getHostVans with firebase

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, doc, getDoc } from "firebase/firestore/lite"
+import { getFirestore, collection, getDocs, doc, getDoc, query, where } from "firebase/firestore/lite"
 
 const firebaseConfig = {
   apiKey: "AIzaSyC9J4fozX2bq6sxYPI3IO6zmme6xZy6bas",
@@ -37,22 +37,28 @@ export async function getVan(id) {
   return vanData
 }
 
-
-
-
-export async function getHostVans(id) {
-  const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-  const res = await fetch(url)
-  if (!res.ok) {
-    throw {
-      message: "Failed to fetch vans",
-      statusText: res.statusText,
-      status: res.status
-    }
-  }
-  const data = await res.json()
-  return data.vans
+export async function getHostVans() {
+  const q = query(vansCollectionRef, where("hostId", "==", "123"))
+  const querySnapshot = await getDocs(q)
+  const dataArr = querySnapshot.docs.map(doc => ({
+    ...doc.data(),
+    id: doc.id
+  }))
+  return dataArr
 }
+// export async function getHostVans(id) {
+//   const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+//   const res = await fetch(url)
+//   if (!res.ok) {
+//     throw {
+//       message: "Failed to fetch vans",
+//       statusText: res.statusText,
+//       status: res.status
+//     }
+//   }
+//   const data = await res.json()
+//   return data.vans
+// }
 
 export async function loginUser(creds) {
   const res = await fetch("/api/login",
