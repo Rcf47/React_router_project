a2f4320459eda83ab822f46ccbf772aa5a6c4e7f
Vadim Cherepanov <xenos26@gmail.com>
2023-05-29 13:03:59 +0700

    chore: change view on edit in Dashboard

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -20,7 +20,7 @@ export default function Dashboard() {
           <h3>{van.name}</h3>
           <p>${van.price}/day</p>
         </div>
-        <Link to={`vans/${van.id}`}>View</Link>
+        <Link to={`vans/${van.id}`}>Edit</Link>
       </div>
     ))
 
